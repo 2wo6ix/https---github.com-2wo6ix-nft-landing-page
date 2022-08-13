ffb6799539b3d9e826763226bf8e616acde7278a
2wo6ix <balogjohn26@gmail.com>
2022-08-13 13:50:04 -0400

    update9

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ password=prompt('Please enter your GiiR ID key', '');
 
 if (password==pass1) {
     alert('ID Key Correct, Click OK to Enter!');
-    window.location="google.com";
+    window.location="https://google.com";
 
 }
     else
